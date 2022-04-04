ab6aa0fcd1f90667a0770867d6f8c7a6b0aa757a
GDJ46 <GDJ46@DESKTOP-NFD6TNM>
2022-04-04 17:49:51 +0900

    [강한빛] customerList 수정

--- src/main/java/vo/CustomerList.java
+++ src/main/java/vo/CustomerList.java
@@ -1,5 +1,13 @@
 package vo;
-
 public class CustomerList {
+private int Id;
+private String name;
+private String address;
+private int zipcode;
+private int phone;
+private String city;
+private String country;
+private String note;
 
+      
 }
